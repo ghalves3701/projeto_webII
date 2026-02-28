0d70406217ff77d7152fa4364b95eb5bff3a7a0f
ghalves3701 <ghcarvalhoalves@gmail.com>
2026-02-27 21:08:21 -0300

    instalando mysql

--- projeto/mkdir_projeto_I/app.js
+++ projeto/mkdir_projeto_I/app.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 3010
 
 // criação da nota raiz
 
 app.get('/', (reg, res) => {
     console.log("Rota Raiz encontrada!")
     res.send("<br><h1>Saudações! </h1>" + 
-            "<br><h2> Rota Raiz encontrada</h2>" +
+            "<br><h2> Rota Raiz encontrada bla</h2>" +
             "<br><h2> Bem vindo ao servidor Express</h2>")
 })
 
@@ -27,6 +27,16 @@ app.get('/cidade_natal/localizacao', (reg, res) => {
             "<br><h2> Localização </h2>" +
             "<br><h2>  Sul de Minas </h2>")
 })
+
+
+app.get('/cidade_natal/turismo', (reg, res) => {
+    console.log("Rota cidade_natal/turismo encontrada!")
+    res.send("<br><h1>Saudações! </h1>" + 
+            "<br><h2> Gruta dos Palhares </h2>" +
+            "<br><h2>  Cachoeiras do César e do Azulinho </h2>")
+})
+
+
 
 //instanciar o servidor web
 
